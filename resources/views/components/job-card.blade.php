102853512c00b74f1a1915248fd2dffcc8a0fde7
unknown <hrayrashchyan94@gmail.com>
2024-07-17 18:51:55 +0400

    frontend completed

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<x-panel class="flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration650">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
+    </div>
+    <div>
+        <div class="flex justify-between items-center mt-auto">
+            <div>
+                <x-tag sizi="small">Backend</x-tag>
+                <x-tag sizi="small">Frontend</x-tag>
+                <x-tag sizi="small">Manager</x-tag>
+            </div>
+            <x-employer-logo :width="42" />
+        </div>
+    </div>
+</x-panel>
